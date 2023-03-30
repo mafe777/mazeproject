296002139cb7c8980eeff67b58abfb816bb68359
mafe <Usuario@DESKTOP-PDI6DA1>
2023-03-29 19:49:59 -0500

    Add project files.

---
+++ Maze.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using maze.logic;
+
+//Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+var maze = new MyMaze(30, 100);
+Console.Write(maze);
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
